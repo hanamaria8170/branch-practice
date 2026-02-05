c5be25922bbfe1aa3c145d3ab99d173bebb87a2e
toptems <hanamaria8170@gmail.com>
2026-02-05 13:37:07 +0900

    feat: Print fizzbuzz

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # 1~n 까지 양의 정수에 대해,
 # 3의 배수, 'fizz'
 # 5의 배수, 'buzz'
-
+# 15의 배수, 'fizzbuzz'
 for i in range(1, 15+1):
+    if i % 15 == 0:
+        print('fizzbuzz')
     if i % 3 == 0:
         print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
     else:
         print(i)
 
