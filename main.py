1a26685b0bfd9ebd7d0759b159692d257c32089e
toptems <hanamaria8170@gmail.com>
2026-02-05 13:14:07 +0900

    feat: Print fizz

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello")
+# fizzbuzz
+# 1~n 까지 양의 정수에 대해,
+# 3의 배수, 'fizz'
+# 5의 배수, 'buzz'
+
+for i in range(1, 15+1):
+    if i % 3 == 0:
+        print('fizz')
+    else:
+        print(i)
+
+
